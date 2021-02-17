0a87d36d1f08c61aada96d30e8760ac68fec5791
Bhavna25 <63115179+Bhavna25@users.noreply.github.com>
2021-02-17 23:50:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,44 @@
-import React, { useEffect, useState } from "react";
-import "./styles.css";
-const App = () => {
-  const [time, setTime] = useState("");
-  var date = new Date();
-  let hr = date.getHours();
-  let min = date.getMinutes();
-  let sec = date.getSeconds();
-  let amORpm = "AM";
-  if (hr > 12) {
-    hr = hr - 12;
-    amORpm = "PM";
-  }
-  if (hr === 0 && amORpm === "AM") hr = 12;
-  const updateTime = () => {
-    sec = Number(sec) + 1;
-    if (sec === 60) {
-      sec = 0;
-      min = Number(min) + 1;
-      if (min === 60) {
-        min = 0;
-        hr = Number(hr) + 1;
-        if (hr === 12 && amORpm === "PM") {
-          // hr = 0;
-          amORpm = "AM";
-        } else if (hr === 12 && amORpm === "AM") {
-          amORpm = "PM";
-        }
-        if (hr > 12 && amORpm === "PM") hr = hr - 12;
-      }
+import React, { useState, useEffect } from 'react';
+import '../styles/App.css';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
     }
-    if (min < 10) min = "0" + Number(min);
-    if (sec < 10) sec = "0" + Number(sec);
-    setTime(hr + ":" + min + ":" + sec + " " + amORpm);
-  };
-  useEffect(() => {
-    if (min < 10) min = "0" + min;
-    if (sec < 10) sec = "0" + sec;
-    setTime(hr + ":" + min + ":" + sec + " " + amORpm);
-    let clearSetInterval = setInterval(updateTime, 1000);
-    return () => {
-      clearInterval(clearSetInterval);
-    };
-  }, []);
-  return (
-    <div className="Clock">
-      <h3 id="time">{time}</h3>
-    </div>
-  );
-};
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        let hours = this.state.date.getHours() < 12 ? this.state.date.getHours() : this.state.date.getHours() - 12;
+        let minutes = this.state.date.getMinutes();
+        let seconds = this.state.date.getSeconds();
+        let ampm = this.state.date.getHours() >= 12 ? 'PM' : 'AM';
+        return (
+            <div className="Clock">
+                <h3 id="time">
+                    {hours == 0 ? 12 : hours > 12 ? hours - 12 : hours}:
+                    {minutes > 9 ? minutes : `0${minutes}`}:
+                    {seconds > 9 ? seconds : `0${seconds}`} {ampm}
+                </h3>
+            </div>
+        );
+    }
+}
 
 export default App;
