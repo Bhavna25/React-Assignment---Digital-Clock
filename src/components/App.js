2322600b3b53efd12f2e9ac5fc8fb76480a35eb2
Bhavna25 <63115179+Bhavna25@users.noreply.github.com>
2021-02-17 18:47:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,52 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = { time: new Date() }; // initialise the state
+const App = () => {
+  const [time, setTime] = useState("");
+  var date = new Date();
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  let amORpm = "AM";
+  if (hr > 12) {
+    hr = hr - 12;
+    amORpm = "PM";
   }
+  if (hr === 0 && amORpm === "AM") hr = 12;
+  const updateTime = () => {
+    sec = Number(sec) + 1;
+    if (sec === 60) {
+      sec = 0;
+      min = Number(min) + 1;
+      if (min === 60) {
+        min = 0;
+        hr = Number(hr) + 1;
+        if (hr === 12 && amORpm === "PM") {
+          // hr = 0;
+          amORpm = "AM";
+        } else if (hr === 12 && amORpm === "AM") {
+          amORpm = "PM";
+        }
+        if (hr > 12 && amORpm === "PM") hr = hr - 12;
+      }
+    }
+    if (min < 10) min = "0" + Number(min);
+    if (sec < 10) sec = "0" + Number(sec);
+    setTime(hr + ":" + min + ":" + sec + " " + amORpm);
+  };
+  useEffect(() => {
+    if (min < 10) min = "0" + min;
+    if (sec < 10) sec = "0" + sec;
+    setTime(hr + ":" + min + ":" + sec + " " + amORpm);
+    let clearSetInterval = setInterval(updateTime, 1000);
+    return () => {
+      clearInterval(clearSetInterval);
+    };
+  }, []);
+  return (
+    <div className="Clock">
+      <h3 id="time">{time}</h3>
+    </div>
+  );
+};
 
-  componentDidMount() {
-    // create the interval once component is mounted
-    this.update = setInterval(() => {
-      this.setState({ time: new Date() });
-    }, 1 * 1000); // every 1 seconds
-  }
-
-  componentWillUnmount() {
-    // delete the interval just before component is removed
-    clearInterval(this.update);
-  }
-
-  render() {
-    const { time } = this.state; // retrieve the time from state
-
-    return <div className="Clock">{time.toLocaleTimeString()}</div>;
-  }
-}
 export default App;
-
-// export default App;
